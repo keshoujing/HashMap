9cb1e6f79b3ddb1991239da7c2b20287a15757f0
ke <keshoujing@gmail.com>
2021-10-12 11:51:13 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+class node
+{
+    string key;
+    string value;
+    node* next;
+    node* pre;
+    node(string key_, string value_) : key(key_), value(value_){};
+    node& operator=(const node& node)
+    {
+        key = node.key;
+        value = node.value;
+        next = node.next;
+        pre = node.pre;
+        return *this;
+    }
+};
+
+unsigned int hashFunction(char const* key, int table_size)
+{
+    int b;
+    int count = strlen(key);
+    unsigned int hashCode = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (i % 2 == 0) //even case
+        {
+            b = (hashCode << 7) ^ key[i] ^ (hashCode >> 3);
+        } else //odd case
+        {
+            b = (hashCode << 11) ^ key[i] ^ (hashCode >> 5);
+            b = ~b;
+        }
+        hashCode = hashCode ^ b;
+    }
+    hashCode = hashCode | (1 << 32);
+    return hashCode;
+}
+
+class UnorderedMap
+{
+private:
+    //define your data structure here
+    vector<node*> table;
+
+    //define other attributes e.g. bucket count, maximum load factor, size of table, etc.
+    unsigned int bucketCount;
+    double LF;
+
+public:
+    class Iterator;
+    UnorderedMap(unsigned int bucketCount, double loadFactor);
+    ~UnorderedMap();
+    Iterator begin() const;
+    Iterator end() const;
+    std::string& operator[] (std::string const& key);
+    void rehash();
+    void remove(std::string const& key);
+    unsigned int size();
+    double loadFactor();
+
+    class Iterator
+    {
+        vector<list<string, string>> vec;
+    public:
+        //this constructor does not need to be a default constructor;
+        //the parameters for this constructor are up to your discretion.
+        //hint: you may need to pass in an UnorderedMap object.
+        Iterator(vector<list<string, string>> vec) {this->vec = vec;}
+        Iterator& operator=(Iterator const& rhs) {
+
+        }
+        Iterator& operator++() {}
+        bool operator!=(Iterator const& rhs) {}
+        bool operator==(Iterator const& rhs) {}
+        std::pair<std::string, std::string> operator*() const {}
+        friend class UnorderedMap;
+    };
+};
+
+UnorderedMap::UnorderedMap(unsigned int bucketCount, double loadFactor)
+{
+    this->bucketCount = bucketCount;
+    LF = loadFactor;
+    for (int i = 0; i < bucketCount; ++i)
+        table.push_back(nullptr);
+
+}
+
+UnorderedMap::~UnorderedMap()
+{
+    for (int i = 0; i < table.size(); ++i)
+        delete table.at(i);
+}
+
+UnorderedMap::Iterator UnorderedMap::begin() const
+{
+    return Iterator();
+}
+
+UnorderedMap::Iterator UnorderedMap::end() const
+{
+
+}
+
+std::string& UnorderedMap::operator[] (std::string const& key)
+{
+
+}
+
+void UnorderedMap::rehash()
+{
+
+}
+
+void UnorderedMap::remove(std::string const& key)
+{
+
+}
+
+unsigned int UnorderedMap::size()
+{
+
+}
+
+double UnorderedMap::loadFactor()
+{
+
+}
+
+//implement other operators in Iterator class
+
+//Do not change main()
+int main()
+{
+    int lines = 0, buckets = 0;
+    double maxLoadFactor = 0.0;
+    std::string command = "", ufid = "", name = "", key = "";
+    std::cin >> lines >> buckets >> maxLoadFactor;
+    UnorderedMap myMap = UnorderedMap(buckets, maxLoadFactor);
+    while(lines--)
+    {
+        std::cin >> command;
+        if(command == "hash")
+        {
+            std::cin >> key;
+            const char* convertedKey = key.c_str();
+            std::cout << hashFunction(convertedKey, buckets) << "\n";
+        }
+        else if(command == "insert")
+        {
+            std::cin >> ufid >> name;
+            myMap[ufid] = name;
+        }
+        else if(command == "size")
+        {
+            std::cout << myMap.size() <<"\n";
+        }
+        else if(command == "load")
+        {
+            std::cout << std::fixed << std::setprecision(2) << myMap.loadFactor() <<"\n";
+        }
+        else if(command == "search")
+        {
+            std::cin >> ufid;
+            std::cout << myMap[ufid] << "\n";
+        }
+        else if(command == "traverse")
+        {
+            for (UnorderedMap::Iterator iter = myMap.begin(); iter != myMap.end(); ++iter)
+            {
+                std::cout << (*iter).first << " " << (*iter).second << "\n";
+            }
+
+            /* This should also work
+                for (auto tableEntry: myMap)
+                {
+                    std::cout << tableEntry.first << " " << tableEntry.second << "\n";
+                }
+            */
+        }
+        else if(command == "remove")
+        {
+            std::cin >> ufid;
+            myMap.remove(ufid);
+        }
+    }
+
+    return 0;
+}
